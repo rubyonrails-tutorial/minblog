134549e3de16f37ff84ed6d15d10a55b8128af89
bredouane <billel.redouane@gmail.com>
2016-04-10 12:40:48 -0400

    Add fonctionnel test for validation spec MinBlog

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "LayoutLinks", type: :request do
   
+  # Test des routages
   it "devrait trouver une page Home à '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
@@ -24,6 +25,21 @@ RSpec.describe "LayoutLinks", type: :request do
   
   it "devrait avoir une page pour signup a '/signup'" do
     get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+
+  # Vérifient que les routages conduisent effectivement sur les bonnes pages 
+  it "devrait avoir le bon lien sur le layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign up"
     response.should have_selector('title', :content => "Sign up")
   end  
 end
