893d888a64b7dec85a8b149652ca70238c1f966d
bredouane <billel.redouane@gmail.com>
2016-04-10 14:06:43 -0400

    Byebug

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe UsersController, type: :controller do
 
   describe "GET #signup" do
     it "devrait réussir" do
-      get 'users/signup'
+      get :signup
       expect(response).to have_http_status(:success)
     end
 
     it "devrait avoir le titre adéquat" do
-      get 'users/signup'
+      get :signup
       expect(response).should have_selector("title", :content => "Sign up")
     end
   end  
