58cd48f1d36e428f14863955f7be098d3d7678fb
bredouane <billel.redouane@gmail.com>
2016-04-10 14:02:50 -0400

    User test unit

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe UsersController, type: :controller do
 
   describe "GET #new" do
     it "devrait réussir" do
-      get :new
+      get 'signup'
       expect(response).to have_http_status(:success)
     end
 
     it "devrait avoir le titre adéquat" do
-      get :new
+      get 'signup'
       expect(response).should have_selector("title", :content => "Sign up")
     end
   end  
