05f44f4a58609a88a301349140d149d6c8f04414
bredouane <billel.redouane@gmail.com>
2016-04-10 00:28:00 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  match '/contact', :to => 'pages#contact'
-  match '/about',   :to => 'pages#about'
-  match '/help',    :to => 'pages#help'
+  match '/pages/contact', :to => 'pages#contact'
+  match '/pages/about',   :to => 'pages#about'
+  match '/pages/help',    :to => 'pages#help'
 
   root :to => 'pages#home'  
   # The priority is based upon order of creation: first created -> highest priority.
