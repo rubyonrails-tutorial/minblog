b20d1bc3b52b16730af818a2488d8f3cdf44f874
bredouane <billel.redouane@gmail.com>
2016-04-10 12:21:08 -0400

    Add Helper Logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def logo
+     image_tag("open-source-logo.png", :alt => "Application exemple", :class => "round")
+  end
+  
   # Retourner un titre basé sur la page.
   def title
     base_title = "Mini Blog"
