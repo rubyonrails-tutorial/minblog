f2db76d90c502302791136f3b679f36c1b975e0b
bredouane <billel.redouane@gmail.com>
2016-04-10 00:21:11 -0400

    Ajout des Routes nommées

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "LayoutLinks", type: :request do
-  describe "GET /layout_links" do
-    it "works! (now write some real specs)" do
-      get layout_links_index_path
-      expect(response).to have_http_status(200)
-    end
+  
+  it "devrait trouver une page Home à '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+
+  it "devrait trouver une page Contact at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+
+  it "should have an À Propos page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => "About")
+  end
+
+  it "devrait trouver une page Iade à '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "Help")
   end
 end
