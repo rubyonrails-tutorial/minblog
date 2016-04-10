313a41d17f9854bfed9c90a4778783078f72d38e
bredouane <billel.redouane@gmail.com>
2016-04-10 14:05:13 -0400

    Byebug pour test du title

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   render_views
 
-  describe "GET #new" do
+  describe "GET #signup" do
     it "devrait réussir" do
-      get 'signup'
+      get 'users/signup'
       expect(response).to have_http_status(:success)
     end
 
     it "devrait avoir le titre adéquat" do
-      get 'signup'
+      get 'users/signup'
       expect(response).should have_selector("title", :content => "Sign up")
     end
   end  
