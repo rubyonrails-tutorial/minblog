284918756f858846187a4f1975854eebc55ea943
billel.redouane <billel.redouane@gmail.com>
2016-04-11 15:00:43 -0400

    Add annotation Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ActiveRecord::Base
   # Les "attr_accessible" en ete supprimer de puis Rails 4.0 et remplacer de puis les Strong parameters.
   # attr_accessible :name, :email
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20160410235826) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "nom"
+    t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
