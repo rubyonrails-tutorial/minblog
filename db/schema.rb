ba31fc438083eb04cab11978f7978e8b80759d6a
billel.redouane <billel.redouane@gmail.com>
2016-04-10 23:27:11 -0400

    Add model User

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160410235826) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "nom"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
