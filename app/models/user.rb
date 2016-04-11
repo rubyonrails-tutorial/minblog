f3b493f8351ea0ecbdf1e602b53ef0553cd75cb7
bredouane <billel.redouane@gmail.com>
2016-04-11 19:29:13 -0400

    Add test model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,10 @@ class User < ActiveRecord::Base
   # Les "attr_accessible" en ete supprimer de puis Rails 4.0 et remplacer de puis les Strong parameters.
   # attr_accessible :name, :email
   
-  validates_presence_of :name, message: "Le nom de ne dois pas etre vide."
-  validates_uniqueness_of :name, message: "Cette nom doit etre unique."
+  validates_presence_of :name, message: "Le nom ne dois pas etre vide."
+  validates_uniqueness_of :name, message: "Le nom doit etre unique."
   validates_length_of :name, maximum: 20, message: "La longueur maximale du nom est 20 lettre."
   
-  validates_presence_of :email, message: "Le email de ne dois pas etre vide."
+  validates_presence_of :email, message: "Le email ne dois pas etre vide."
   validates_uniqueness_of :email, message: "Cette email doit etre unique"
 end
