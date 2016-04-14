8011e147b91fcf0d2048e1edd7be12fe49eee378
bredouane <billel.redouane@gmail.com>
2016-04-14 16:34:42 -0400

    Add min test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,24 +10,24 @@ RSpec.describe UsersController, type: :controller do
     end
 
     it "devrait réussir" do
-      get 'show', :id => @user
+      get :show, :id => @user
       response.should be_success
     end
 
     it "devrait trouver le bon utilisateur" do
-      get 'show', :id => @user
+      get :show, :id => @user
       assigns(:user).should == @user
     end
   end  
   
   describe "GET #new" do
     it "devrait réussir" do
-      get 'new'
+      get :new
       expect(response).to have_http_status(:success)
     end
 
     it "devrait avoir le titre adéquat" do
-      get 'new'
+      get :new
       expect(response).should have_selector("head title", :content => "Sign up")
     end
   end  
