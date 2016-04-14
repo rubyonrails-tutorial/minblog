03ea54cd5a9714a9dd5f50e44f940e7e0ac45b08
bredouane <billel.redouane@gmail.com>
2016-04-14 15:44:38 -0400

    Add Spec factrories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,19 @@
 # En utilisant le symbole ':user', nous faisons que
 # Factory Girl simule un modèle User.
-Factory.define :user do |user|
-  user.nom                  "Michael Hartl"
-  user.email                 "mhartl@example.com"
-  user.password              "foobar"
-  user.password_confirmation "foobar"
+FactoryGirl.define do
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+#    factory :admin do
+#      admin true
+#    end
+  end
+
+#  factory :micropost do
+#    content "Lorem ipsum"
+#    user
+#  end
 end
