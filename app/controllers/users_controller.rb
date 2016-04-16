24c7b86933c591c6381d616ab5f26d1013a82978
bredouane <billel.redouane@gmail.com>
2016-04-16 19:11:46 -0400

    fix controler create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end  
   
   def create
-    @user = User.find(params[:id])    
+    @user = User.new(params[:user])    
     if @user.save
       # Traite un succès d'enregistrement.
     else
