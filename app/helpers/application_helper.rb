497ff4c1a4e2c8fa26222d1b2f513765438e9687
bredouane <billel.redouane@gmail.com>
2016-04-18 13:26:43 -0400

    Add Helper erreurs messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,19 @@
 module ApplicationHelper
 
   def logo
-     image_tag("logo-minblog.png", :alt => "Application exemple", :class => "round")
+    image_tag("logo-minblog.png", :alt => "Application exemple", :class => "round")
   end
   
   # Retourner un titre basé sur la page.
   def title
-   p base_title = "Mini Blog"
-   p "#{base_title} - #{@title}".html_safe if @titre.nil?
+    p base_title = "Mini Blog"
+    p "#{base_title} - #{@title}".html_safe if @titre.nil?
+  end
+ 
+  def errors_full_messages
+    flash.each do |key, value|
+      errors_message+= "<div class='flash "+key+"'>"+value+"</div>"
+    end
+    errors_message
   end
 end
