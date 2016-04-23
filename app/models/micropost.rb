17124122f260077c0c2043c945bc120b09046829
bredouane <billel.redouane@gmail.com>
2016-04-23 14:10:30 -0400

    Fix default_scope

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,8 +3,8 @@ class Micropost < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :content, :presence => true, :length => { :maximum => 140 }
-  validates :user_id, :presence => true  
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :user_id, presence: true
 
-  default_scope :order => 'microposts.created_at DESC'
+  default_scope -> { order('created_at DESC') }  
 end
