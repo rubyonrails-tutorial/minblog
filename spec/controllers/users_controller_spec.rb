46969228413addd23e40e2c4411de45bb8b72865
bredouane <billel.redouane@gmail.com>
2016-04-23 12:19:32 -0400

    Add rake db:populate for micro-posts

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -34,8 +34,8 @@ RSpec.describe UsersController, type: :controller do
     end
     
     it "devrait afficher les micro-messages de l'utilisateur" do
-      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
-      mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
+      mp1 = FactoryGirl.create(:micropost, :user => @user, :content => "Foo bar")
+      mp2 = FactoryGirl.create(:micropost, :user => @user, :content => "Baz quux")
       get :show, :id => @user
       response.should have_selector("span.content", :content => mp1.content)
       response.should have_selector("span.content", :content => mp2.content)
