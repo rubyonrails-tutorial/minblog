04f8d73a2c66d378bf314ffd8f1758bc394df016
bredouane <billel.redouane@gmail.com>
2016-04-23 21:54:35 -0400

    Add action delete micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,8 +1,9 @@
 class MicropostsController < ApplicationController
   before_action :authenticate, only: [:create, :destroy]
+  before_action :correct_user, :only => [:destroy]
 
   def create
-    @micropost  = current_user.microposts.build(params[:micropost])
+    @micropost  = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_path
@@ -13,5 +14,18 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_back_or root_path
+  end
+
+  private
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_path unless @micropost.nil?
+  end
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
   end
 end
