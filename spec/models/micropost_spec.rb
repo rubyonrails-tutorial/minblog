fe27cc58d3faeeb0965f434520b323a0101bda5c
billel.redouane <billel.redouane@gmail.com>
2016-04-24 23:10:09 -0400

    Add annotation

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer          not null, primary key
+#  content    :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
